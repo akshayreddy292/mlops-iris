77a386285cf1665f12185c6de15f7ff80a8c6d76
akshayreddy292 <86187767+akshayreddy292@users.noreply.github.com>
2021-11-27 20:44:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from ml_utils import load_model, predict, retrain
 from typing import List
+import datetime
 
 # defining the main app
 app = FastAPI(title="Iris Predictor", docs_url="/")
@@ -22,6 +23,7 @@ class QueryIn(BaseModel):
 # class which is returned in the response
 class QueryOut(BaseModel):
     flower_class: str
+    timestamp: str
 
 # class which is expected in the payload while re-training
 class FeedbackIn(BaseModel):
@@ -35,7 +37,8 @@ class FeedbackIn(BaseModel):
 @app.get("/ping")
 # Healthcheck route to ensure that the API is up and running
 def ping():
-    return {"ping": "pong"}
+    ct = datetime.datetime.now().strftime('%d-%B-%y %H:%M')
+    return {"ping": "pong", "timestamp": ct}
 
 
 @app.post("/predict_flower", response_model=QueryOut, status_code=200)
@@ -43,7 +46,8 @@ def ping():
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
 def predict_flower(query_data: QueryIn):
-    output = {"flower_class": predict(query_data)}
+    ct = datetime.datetime.now().strftime('%d-%B-%y %H:%M')
+    output = {"flower_class": predict(query_data), "timestamp": ct}
     return output
 
 @app.post("/feedback_loop", status_code=200)
@@ -52,10 +56,11 @@ def predict_flower(query_data: QueryIn):
 # Response: Dict with detail confirming success (200)
 def feedback_loop(data: List[FeedbackIn]):
     retrain(data)
-    return {"detail": "Feedback loop successful"}
+    ct = datetime.datetime.now().strftime('%d-%B-%y %H:%M')
+    return {"detail": "Feedback loop successful", "timestamp": ct}
 
 
 # Main function to start the app when main.py is called
 if __name__ == "__main__":
     # Uvicorn is used to run the server and listen for incoming API requests on 0.0.0.0:8888
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8889, reload=True)
