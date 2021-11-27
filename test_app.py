c08bceeb7d155c92d0293ba13df93f23838359d9
akshayreddy292 <86187767+akshayreddy292@users.noreply.github.com>
2021-11-27 20:42:54 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -14,13 +14,46 @@ def test_ping():
 def test_pred_virginica():
     # defining a sample payload for the testcase
     payload = {
-        "sepal_length": 3,
-        "sepal_width": 5,
-        "petal_length": 3.2,
-        "petal_width": 4.4,
+        "sepal_length": 6.9,
+        "sepal_width": 3.1,
+        "petal_length": 5.1,
+        "petal_width": 2.3,
     }
     with TestClient(app) as client:
         response = client.post("/predict_flower", json=payload)
+        ct = datetime.datetime.now().strftime('%d-%B-%y %H:%M')
         # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"flower_class": "Iris Virginica"}
+        assert response.json() == {"flower_class": "Iris Virginica", "timestamp": ct}
+        
+# test to check if Iris Versicolour is classified correctly
+def test_pred_versicolor():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 7.0,
+        "sepal_width": 3.2,
+        "petal_length": 4.7,
+        "petal_width": 1.4,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        ct = datetime.datetime.now().strftime('%d-%B-%y %H:%M')
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour", "timestamp": ct}
+        
+# test to check if Iris Setosa is classified correctly
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        ct = datetime.datetime.now().strftime('%d-%B-%y %H:%M')
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa", "timestamp": ct}
